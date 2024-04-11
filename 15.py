af31c782f9260c843ebecee421f3956cc15be846
Yao Zhang <lovely.yoyo@live.cn>
2024-04-11 10:41:18 -0700

    Update 15.py

--- 15.py
+++ 15.py
@@ -24,7 +24,7 @@ class Solution(object):
                     r -= 1
                     while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                    while l < r and nums[r] == nums[r - 1]:
+                    while l < r and nums[r] == nums[r + 1]:
                         r -= 1
         return res
 
