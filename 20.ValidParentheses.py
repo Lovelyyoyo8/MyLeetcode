6356e18349794c978502ce3bc75b2deb2ca9550f
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-04-23 11:19:55 -0700

    Update 20.ValidParentheses.py

--- 20.ValidParentheses.py
+++ 20.ValidParentheses.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        replace = True
+        while replace:
+            start_length = len(s)
+            for inner in ["{}", "()", "[]"]:
+                s = s.replace(inner, "")
+            if start_length == len(s):
+                replace = False
+       
+        return s == ""
 
