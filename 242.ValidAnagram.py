7892a94acbb83b314da02e0d514d5087ea96476a
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-05-08 09:55:59 -0700

    Update 242.ValidAnagram.py

--- 242.ValidAnagram.py
+++ 242.ValidAnagram.py
@@ -1,1 +1,24 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
 
+        s_dict = {}
+        for char in s:
+            if char in s_dict:
+                s_dict[char] += 1
+            else:
+                s_dict[char] = 1
+                
+        for char in t:
+            if char not in s_dict or s_dict[char] == 0:
+                return False
+            s_dict[char] -= 1
+        
+        return True
+    
