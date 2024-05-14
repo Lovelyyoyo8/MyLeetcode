6b3adfcde3731b2df27e88f09c7d31a20426992b
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-05-14 12:15:33 -0700

    Update 733.FloodFill.py

--- 733.FloodFill.py
+++ 733.FloodFill.py
@@ -1,1 +1,24 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+        cur_color = image[sr][sc]
+        height = len(image)
+        width = len(image[0])
 
+        def dfs(sr, sc):
+            if 0 <= sr < height and 0 <= sc < width and image[sr][sc] == cur_color and image[sr][sc] != color:
+                image[sr][sc] = color
+                dfs(sr+1, sc)
+                dfs(sr-1, sc)
+                dfs(sr, sc+1)
+                dfs(sr, sc-1)
+        
+        dfs(sr, sc)
+
+        return image
