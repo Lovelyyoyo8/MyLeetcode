f84112e0a1edd001f271a15cc06029459d234bf9
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-05-21 11:01:20 -0700

    Update 1572.MatrixDiagonalSum.py

--- 1572.MatrixDiagonalSum.py
+++ 1572.MatrixDiagonalSum.py
@@ -1,1 +1,13 @@
+class Solution(object):
+    def diagonalSum(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: int
+        """
+        res, n = 0, len(mat)
 
+        for i in range(n):
+            res += mat[i][i]  
+            res += mat[i][len(mat) -1 -i]
+
+        return res - (mat[n // 2][n // 2] if n%2 else 0)
