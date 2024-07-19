3143badca0c80884abedda91a5aaf1f7173086dc
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-07-19 13:57:23 -0700

    Update 409.LongestPalindrome.py

--- 409.LongestPalindrome.py
+++ 409.LongestPalindrome.py
@@ -1,1 +1,24 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        from collections import Counter
+
+        count = Counter(s)
+        res = 0
+        odd_found = False
+
+        for cnt in count.values():
+            if cnt % 2 == 0:
+                res += cnt
+            else:
+                res += cnt - 1
+                odd_found = True
+
+        if odd_found:
+            res += 1
+
+        return res
 
