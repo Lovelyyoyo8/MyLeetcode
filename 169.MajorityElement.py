a9bdf53711ccd506b71303667dedd8caa691e8a0
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-07-22 11:33:30 -0700

    Update 169.MajorityElement.py

--- 169.MajorityElement.py
+++ 169.MajorityElement.py
@@ -1,1 +1,15 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        maj = 0
 
+        for num in nums:
+            if count == 0:
+                maj = num
+            count += (1 if num == maj else -1)
+
+        return maj
