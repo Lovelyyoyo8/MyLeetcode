688b5a0a664a446a03bed60ea64cc98d13336c15
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-07-24 12:07:30 -0700

    Update 67.AddBinary.py

--- 67.AddBinary.py
+++ 67.AddBinary.py
@@ -1,1 +1,27 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+        result = ""
+
+    while i >= 0 or j >= 0:
+        sum = carry
+        if i >= 0:
+            sum += int(a[i])
+            i -= 1
+        if j >= 0:
+            sum += int(b[j])
+            j -= 1
+        result = str(sum % 2) + result
+        carry = sum // 2
+
+    if carry:
+        result = '1' + result
+
+    return result
 
