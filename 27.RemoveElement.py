4144fd9e9e486514bb48306fdf6e4b06b8be2e30
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-08-29 14:46:55 -0700

    Update 27.RemoveElement.py

--- 27.RemoveElement.py
+++ 27.RemoveElement.py
@@ -1,1 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        next_pos = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[next_pos] = nums[i]
+                next_pos += 1
+        
+
+        return next_pos
+
 
