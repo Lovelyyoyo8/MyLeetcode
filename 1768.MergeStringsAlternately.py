43d56af311b99d8bb24fda222a5e5410be7d5136
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-10-17 19:04:44 -0700

    Update 1768.MergeStringsAlternately.py

--- 1768.MergeStringsAlternately.py
+++ 1768.MergeStringsAlternately.py
@@ -1,1 +1,26 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+
+        merged = []
+        i, j = 0, 0
+
+        # Loop through both strings and alternate between characters
+        while i < len(word1) and j < len(word2):
+            merged.append(word1[i])
+            merged.append(word2[j])
+            i += 1
+            j += 1
+
+        # Append the remaining part of the longer string
+        if i < len(word1):
+            merged.append(word1[i:])
+        if j < len(word2):
+            merged.append(word2[j:])
+        
+        return ''.join(merged)
 
