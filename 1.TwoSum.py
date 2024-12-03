52ae6efc5170458e11d4514a5a801db5ef45bb92
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2024-12-03 13:38:10 -0800

    Update 1.TwoSum.py

--- 1.TwoSum.py
+++ 1.TwoSum.py
@@ -11,3 +11,23 @@ class Solution(object):
                    return([i,j])
 
 # I feel like I have to do this one anyway.#
+
+
+For Python 3: 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+Hashing Map: 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}  # Dictionary to store the number and its index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
