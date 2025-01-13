83a87e2e05084d0a2490f04c4139f2c70d09ccdc
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-01-13 10:26:11 -0800

    Update 242.ValidAnagram.py

--- 242.ValidAnagram.py
+++ 242.ValidAnagram.py
@@ -22,3 +22,13 @@ class Solution(object):
         
         return True
     
+# Second solution which is simplyer
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_dict = Counter(s)
+        t_dict = Counter(t)
+
+        return s_dict == t_dict
