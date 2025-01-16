683c268288301b8648b0f4f0e50f261f3bd2f807
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-01-16 15:01:09 -0800

    Update 205.IsomorphicStrings.py

--- 205.IsomorphicStrings.py
+++ 205.IsomorphicStrings.py
@@ -1,11 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         mapST, mapTS = {}, {}
-
-        for c1, c2 in zip(s, t):
+#hashing map
+        
+        for c1, c2 in zip(s, t):  #zip function
             if ((c1 in mapST and mapST[c1] != c2) or
                 (c2 in mapTS and mapTS[c2] != c1)):
                 return False
-            mapST[c1] = c2
+            mapST[c1] = c2   #exchange
             mapTS[c2] = c1
         return True
