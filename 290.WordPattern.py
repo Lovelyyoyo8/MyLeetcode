6f0767835584fc920a717bc1cbfa65138ed8fcb0
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-01-17 10:38:26 -0800

    Update 290.WordPattern.py

--- 290.WordPattern.py
+++ 290.WordPattern.py
@@ -18,4 +18,4 @@ class Solution:
     
         return True
 
-#similar to 205, check length first, use zip to pair up
+#similar to 205, check length first, use zip to pair up, use "word" not "w", for clarity
