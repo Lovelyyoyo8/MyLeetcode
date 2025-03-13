d5e7806379a8e9904d559acc5aee69f5d21ccc64
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-03-13 10:22:33 -0700

    Update 58.LengthofLastWord.py

--- 58.LengthofLastWord.py
+++ 58.LengthofLastWord.py
@@ -1,1 +1,15 @@
+#My first write it!
 
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        word = s.strip().split() #got helped by GPT
+
+        if len(s) == 0:
+            return 0
+        else: 
+            count = len(word[-1])
+            return count
+
+#ChatGPT use only 1 line, but not consider 0 condition separately, which also work.
+def lengthOfLastWord(s: str) -> int:
+    return len(s.strip().split()[-1])
