d69a1519e783138560481ff04ebf15758cac96b2
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-04-08 10:57:55 -0700

    Update 344.ReverseString.py

--- 344.ReverseString.py
+++ 344.ReverseString.py
@@ -5,3 +5,11 @@ class Solution(object):
         :rtype: None Do not return anything, modify s in-place instead.
         """
         s[:] = s[::-1]
+
+#In Python3:
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
