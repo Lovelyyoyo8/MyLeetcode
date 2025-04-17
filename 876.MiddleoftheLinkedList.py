ef5d55df3f8078fb5dd228667e3037aad758309e
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-04-17 10:18:33 -0700

    Update 876.MiddleoftheLinkedList.py

--- 876.MiddleoftheLinkedList.py
+++ 876.MiddleoftheLinkedList.py
@@ -15,3 +15,23 @@ class Solution(object):
             slow = slow.next
             fast = fast.next.next
         return slow
+
+#In Python3:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head  # start from head
+        fast = head  # both pointers start from head
+
+        # Move fast by 2 steps and slow by 1 step
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # when fast reaches end, slow is at middle
+        return slow
