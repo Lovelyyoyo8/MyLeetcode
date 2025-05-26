2ff366d38ad3b175e9cc7cfc4dee52e7b1c07f34
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-05-26 11:56:23 -0700

    Update 367.ValidPerfectSquare.py

--- 367.ValidPerfectSquare.py
+++ 367.ValidPerfectSquare.py
@@ -1,1 +1,18 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        # Binary search range: from 1 to num
+        left, right = 1, num
+        
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+            
+            if square == num:
+                return True  # Found perfect square
+            elif square < num:
+                left = mid + 1  # Move right to find bigger square
+            else:
+                right = mid - 1  # Move left to find smaller square
+                
+        return False  # No perfect square found
 
