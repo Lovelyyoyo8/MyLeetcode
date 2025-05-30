a1d4d8815c1e4b502ba28b3810f13fe899a54975
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-05-30 11:00:40 -0700

    Update 278.FirstBadVersion.py

--- 278.FirstBadVersion.py
+++ 278.FirstBadVersion.py
@@ -26,3 +26,20 @@ class Solution(object):
         
         return -1
         
+#In Python3, which is shorter
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid  # first bad is at or before mid
+            else:
+                left = mid + 1  # first bad is after mid
+        
+        return left  # or right, since they are equal
