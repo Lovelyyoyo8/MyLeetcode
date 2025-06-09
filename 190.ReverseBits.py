82773e5eebcfc139345377f4bb47b7c68d2bd409
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-06-09 13:30:36 -0700

    Update 190.ReverseBits.py

--- 190.ReverseBits.py
+++ 190.ReverseBits.py
@@ -13,6 +13,7 @@ class Solution:
 class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
+        
         for i in range(32):
             bit = n & 1               # Get the last bit of n
             result = (result << 1) | bit  # Shift result left and add the bit
