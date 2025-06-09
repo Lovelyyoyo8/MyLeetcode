4a9ff964e30d821946356ad1eef96314042db597
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-06-09 13:30:24 -0700

    Update 190.ReverseBits.py

--- 190.ReverseBits.py
+++ 190.ReverseBits.py
@@ -8,3 +8,14 @@ class Solution:
             bit = (n >> i) & 1
             res = res | (bit << (31 - i))
         return res
+
+#In Python3:
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        for i in range(32):
+            bit = n & 1               # Get the last bit of n
+            result = (result << 1) | bit  # Shift result left and add the bit
+            n >>= 1                  # Shift n right to process next bit
+        return result
+
