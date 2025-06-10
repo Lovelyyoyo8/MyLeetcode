3a1cb558f4f48c96499913d0cc193634558aa89e
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-06-10 09:39:18 -0700

    Update 191.NumberOf1Bits.py

--- 191.NumberOf1Bits.py
+++ 191.NumberOf1Bits.py
@@ -18,3 +18,12 @@ class Solution:
             n &= n - 1  # removes the lowest set bit
             count += 1
         return count
+
+#
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        for _ in range(32):  # because it's a 32-bit integer
+            count += n & 1    # check if the last bit is 1
+            n >>= 1           # right shift n by 1
+        return count
