38a36bc8f200779272794aec35f1e89e99f6ce62
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-06-10 09:38:48 -0700

    Update 191.NumberOf1Bits.py

--- 191.NumberOf1Bits.py
+++ 191.NumberOf1Bits.py
@@ -9,3 +9,12 @@ class Solution(object):
             n &= (n-1)
             res += 1
         return res
+
+#In Python3:
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            n &= n - 1  # removes the lowest set bit
+            count += 1
+        return count
