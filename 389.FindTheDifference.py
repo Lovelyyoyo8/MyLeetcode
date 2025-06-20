4f421678d19c3f615157727b617e7dfc243a34c2
yaozh2025 <119758674+Lovelyyoyo8@users.noreply.github.com>
2025-06-20 09:43:51 -0700

    Update 389.FindTheDifference.py

--- 389.FindTheDifference.py
+++ 389.FindTheDifference.py
@@ -1,1 +1,10 @@
+#The second XOR question, after 136
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        result = 0
+        for char in s:
+            result ^= ord(char)
+        for char in t:
+            result ^= ord(char)
+        return chr(result)
 
